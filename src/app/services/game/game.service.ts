b5c07602ed8e37424cc5a88340b84fa8d2df48b1
Morgan <morgan.fromentin@gmail.com>
2023-01-12 14:54:32 +0100

    displaying pending games in homeComponent

--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -6,6 +6,9 @@ import { RestCallService } from '../restcall/rest-call.service';
 import { Router } from '@angular/router';
 import { GAME } from '../../constants';
 import { GameModel } from '../../models/game.model';
+import { WindowService } from '../window/window.service';
+import { GuidUtils } from '../../utils/guid.utils';
+import { ApiAuthService } from '../auth/auth.service';
 
 @Injectable()
 export class GameService extends ApiClass {
@@ -15,7 +18,28 @@ export class GameService extends ApiClass {
         opponent: GAME.OPPONENT.COMPUTER
     };
 
+    constructor(public http: HttpClient,
+                private restCallService: RestCallService,
+                private router: Router,
+                private windowService: WindowService,
+                private apiAuthService: ApiAuthService) {
+        super(http);
+    }
+
     public initNextGame(options: GameModel | null) {
         this.nextGame = {...options};
     }
+
+    public getAllGames(): Promise<GameModel[]> {
+        this.requesting = true;
+
+        const restCall: Promise<GameModel> = this.http
+          .get<GameModel>(`${environment.BACKEND_URL}/games`, {
+            headers: this.getHeaders(),
+            responseType: ReqContentType.Json
+          }).toPromise();
+
+        return this.restCallService.doRestCall(restCall, null,
+          {onComplete: this.defaultOnCompete.bind(this), onError: this.handleError.bind(this)});
+      }
 }
--- src/app/views/pages/main/components/home/home.component.ts
+++ src/app/views/pages/main/components/home/home.component.ts
@@ -1,34 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { MdbTableDirective, MdbTablePaginationComponent } from 'angular-bootstrap-md';
 import { GAME } from '../../../../../constants';
 import { GameService } from 'src/app/services/game/game.service';
 import { GameModel } from 'src/app/models/game.model';
+import { ApiAuthService } from 'src/app/services/auth/auth.service';
+import { JwtDecoded } from 'src/app/interfaces/JwtDecoded.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild(MdbTablePaginationComponent, {static: true}) mdbTablePagination: MdbTablePaginationComponent;
+  @ViewChild(MdbTableDirective, {static: true}) mdbTable: MdbTableDirective;
+  elements: GameModel[] = [];
+  previous: GameModel[] = [];
   searchText: string = '';
-  headElements = ['Player\'s name', 'Mode', 'Rounds', 'Actions'];
+  headElements = ['Creator', 'Opponent', 'Mode', 'Rounds', 'Actions'];
 
   roundNumber = 3;
   mode = GAME.MODE.CLASSIC;
   opponent = GAME.OPPONENT.COMPUTER;
   MODES = GAME.MODE;
   OPPONENTS = GAME.OPPONENT;
+  connectedUser;
 
   constructor(private router: Router,
-              private gameService: GameService) {
+              private cdRef: ChangeDetectorRef,
+              private gameService: GameService,
+              private apiAuthService: ApiAuthService,) {
   }
 
   ngOnInit() {
+    const decoded: JwtDecoded = this.apiAuthService.getJwtTokenDecoded();
+    this.connectedUser = decoded;
 
+    this.gameService.getAllGames()
+      .then(data => {
+        this.elements = data;
+        this.mdbTable.setDataSource(this.elements);
+
+        this.elements = this.mdbTable.getDataSource();
+        this.previous = this.mdbTable.getDataSource();
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(5);
+
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+
+    this.cdRef.detectChanges();
   }
 
   public startGame(): void {
     this.gameService.initNextGame(<GameModel>{roundNumber: this.roundNumber, mode: this.mode, opponent: this.opponent});
     this.router.navigate([`/main/game`]);
+  }
+
+  displayGameMode(mode: string): string {
+    return GAME.MODE[mode.toUpperCase()];
+  }
+
+  displayGameOpponent(opponent: string): string {
+    return GAME.OPPONENT[opponent.toUpperCase()];
+  }
+
+  displayGameCreator(creator: number, creatorUsername: string): string {
+    return this.connectedUser.username === creatorUsername ? 'me' : creatorUsername;
   }
 }
